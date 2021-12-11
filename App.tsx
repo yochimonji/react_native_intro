fbf069b6f86142b89c29109432941c683df193ba
yochimonji <yochimonji@gmail.com>
2021-12-12 00:35:25 +0900

    add ユーザー一覧の取得

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import tw from "twrnc";
 
 import Header from "./components/Header";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const users = await res.json();
+      setUsers(users);
+    };
+    getUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header title="ユーザー一覧" />
+      {users.map((user) => (
+        <Text key={user.id}>{user.name}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
